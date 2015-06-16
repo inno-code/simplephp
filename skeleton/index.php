8ae5ffe5ff30eaaf8beaf68aff9047aab2094947
taono <taotang@icloud.com>
2015-06-16 13:38:05 -0400

    Assignment submission 1
    * added an index page.php

---
+++ skeleton/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>index page</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p> I'm not sure if index shall have a standard html  <br />
+    page. I will leave it blank if I shouldn't. For now I'll leave it as it is. 
+</p>
+
+</body>
+</html>
