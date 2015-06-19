05f3093fdf5365fb7577fe0e9ccf22a27e2ae36d
taono <taotang@icloud.com>
2015-06-19 17:15:19 -0400

    Thrid Draft
    * added form and handle form. The login page doesn't display name and email properly. I will double check.
    * treid to add rebox to the homepage. it doesn't work well.

---
+++ skeleton/handle_login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
--- skeleton/index.php
+++ skeleton/index.php
@@ -18,7 +18,14 @@
 <div class="parabox">
     <p>This website is dedicated to practing php, html, CSS, and other basics <br />
     to build a simple website. The current version of this website is more of <br />
-    a structure than contents.
+    a structure than contents.<br /> Welcome to add any comments, suggestions, and awesomeness to the redbox below.
+    
+    </p>
+</div>
+
+<div class="redbox">
+    <p>
+       I'm a red box. 
     </p>
 </div>
 
