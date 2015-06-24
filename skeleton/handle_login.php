e7991233ac4f60c9eed044c77a6b154eb2333a81
taono <taotang@icloud.com>
2015-06-24 17:32:36 -0400

    Modified handle form
    * modified handle form.
    * new handle form in studyphp doesn't work. need to fix it.

--- skeleton/handle_login.php
+++ skeleton/handle_login.php
@@ -3,7 +3,11 @@
 <html>
 <body>
 
-Welcome <?php echo $_GET["name"]; ?><br>
+Welcome <?php
+
+ini-set('display_errors',1);
+error_reporting (E_ALL | E_STRICT);
+echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
 
 </body>
